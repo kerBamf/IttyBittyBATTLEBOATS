17625367498f55fdfb20f3aff294f34678b430b8
kerBamf <zach.pedigo@gmail.com>
2023-02-28 19:17:40 -0500

    Add player boat selection logic

--- app.js
+++ app.js
@@ -7,9 +7,13 @@
 const allTiles = document.querySelectorAll('.tile')
 const enRows = document.querySelectorAll('.enRow')
 const playRows = document.querySelectorAll('.playRow')
+let selectPhase = true;
+let playerTurn = true;
 let enGridArray = [];
 let playGridArray = [];
 let computerBrainArray = [];
+let playerBoatCount = 0;
+
 
 //Building Class for tiles. Each tile will have boat stats, but boat presence will be toggled true or false at the beginning of the game
 
@@ -18,6 +22,7 @@ class TileStats {
         this.boatPresent = false;
         this.health = 3;
         this.sighted = false;
+        this.tileChecked = false;
     }
 }
 
@@ -41,14 +46,28 @@ buildTileArray()
 function playTileListeners() {
 
     for(let i = 0; i < playRows.length; i++) {
-        let tileArray = playRows[i].children;
+        let tiles = playRows[i].children;
         for (let j = 0; j < tiles.length; j++) {
-            tileArray[j].addEventListener('click', function() {
-                
+            tiles[j].addEventListener('click', function() {
+               if (selectPhase == true) {
+                    selectBoatPosition(i, j)
+               }
+               
             })
         }
     }
 }
+
+//Player Tile Logic
+
+function selectBoatPosition(row, tile) {
+    playGridArray[row][tile].boatPresent = true;
+    console.log(playGridArray[row][tile].boatPresent);
+    playRows[row].children[tile].style.backgroundColor = "grey";
+    playerBoatCount = playerBoatCount + 1
+    console.log(playerBoatCount);
+}
+
 
 // New Listener currently populated with hit/miss logic, deriving coordinates from iterators. This will allow listeners to be accurately applied regardless of grid size, and will match associated boat-stat array(s) since they both use the same iterators. Later version will execute different functions based on "game-state" variables yet to be coded.
 function enTileListeners() {
@@ -57,14 +76,10 @@ function enTileListeners() {
         let tiles = enRows[i].children;
         for (let j = 0; j < tiles.length; j++) {
             tiles[j].addEventListener('click', function() {
-               let boat = enGridArray[i][j];
-               console.log(`Shots fired at ${i}, ${j}`);
-               if (boat.boatPresent == true){
-                boat.health -= 1;
-                console.log(enGridArray[i][j].health)
-               } else {
-                console.log('Miss')
+               if (playerTurn == true) {
+                playerOffensive(i, j)
                }
+               
             })
         }
     }
@@ -72,3 +87,15 @@ function enTileListeners() {
 
 playTileListeners()
 enTileListeners()
+
+//Function defining enemy tile behavior during player turn
+function playerOffensive(row, tile) {
+    let boat = enGridArray[row][tile];
+    console.log(`Shots fired at ${row}, ${tile}`);
+    if (boat.boatPresent == true){
+        boat.health -= 1;
+        console.log(enGridArray[row][tile].health)
+    } else {
+        console.log('Miss')
+    }
+}
